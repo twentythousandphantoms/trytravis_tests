20315fb1e51038a7c09e6c266e51771bacfd3670
Andrey Belov <nsevenx@gmail.com>
2018-03-11 20:40:05 +0300

    test.py added

---
+++ play-travis/test.py
@@ -0,0 +1,9 @@
+<HTML>
+<HEAD>
+<TITLE>Moved Permanently</TITLE>
+</HEAD>
+<BODY BGCOLOR="#FFFFFF" TEXT="#000000">
+<H1>Moved Permanently</H1>
+The document has moved <A HREF="https://raw.githubusercontent.com/express42/otus-snippets/99713e3f351ada5162829b84be3cc3081b0270ec/play-travis/test.py">here</A>.
+</BODY>
+</HTML>
